6a4a9a75bded744bb36efce0a25088d3c0290685
Your Name <you@example.com>
2022-11-26 22:13:58 +0200

    x

--- sfsf.java
+++ sfsf.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication3;
+package sfsf;
 
 /**
  *
  * @author youssef
  */
-public class JavaApplication3 {
+public class sfsf {
 
     /**
      * @param args the command line arguments
